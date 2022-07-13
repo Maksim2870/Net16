a74706eb941eba5a105a2f2d746b09691b96b732
Maxim Babich <babich.maxim@bk.ru>
2022-07-13 21:28:40 +0300

    add test

--- Test/Program.cs
+++ Test/Program.cs
@@ -20,8 +20,8 @@ namespace Test
                                   "Enter 1 to enter name" + Environment.NewLine +
                                   "Enter 2 to enter surname" + Environment.NewLine +
                                   "Enter 3 to enter age" + Environment.NewLine +
-                                  "Enter -1 tp exit" + Environment.NewLine +
-                                  "Enter -2 tp print exit");
+                                  "Enter 4 tp exit" + Environment.NewLine +
+                                  "Enter 5 tp print exit");
                 response = int.Parse(Console.ReadLine());
                 switch (response)
                 {
@@ -56,17 +56,26 @@ namespace Test
                             age = int.Parse(Console.ReadLine());
                             break;
                         }
+                    case -4:
+                        {
 
-                     default:
+                            Console.WriteLine("enter");
+                            age = int.Parse(Console.ReadLine());
+                            break;
+                        }
+                    case -5:
+                        {
+
+                            Console.WriteLine("print exit");
+                            age = int.Parse(Console.ReadLine());
+                            break;
+                        }
+
+                    default:
                         {
                             Console.WriteLine("You entered wrong number");
                             break;
                         }
-
-
-
-
-
 
                 }
                 

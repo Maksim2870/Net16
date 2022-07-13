f095f204115bc9c5d322770bb32463110e0b0c6b
Maxim Babich <babich.maxim@bk.ru>
2022-07-13 21:44:12 +0300

    add test

--- Test/Program.cs
+++ Test/Program.cs
@@ -14,27 +14,28 @@ namespace Test
             string surname = "Babich";
             int age = 42;
             int response = 0;
+            int haight = 0;
             while (response != -1)
             {
                 Console.WriteLine("Enter 0 to enter your name" + Environment.NewLine +
                                   "Enter 1 to enter name" + Environment.NewLine +
                                   "Enter 2 to enter surname" + Environment.NewLine +
                                   "Enter 3 to enter age" + Environment.NewLine +
-                                  "Enter 4 tp exit" + Environment.NewLine +
-                                  "Enter 5 tp print exit");
+                                  "Enter -1 to enter haight" + Environment.NewLine +
+                                  "Enter -2 to print exit");
                 response = int.Parse(Console.ReadLine());
                 switch (response)
                 {
 
 
-                    case -0:
+                    case 0:
                         {
                             Console.WriteLine("enter your name");
                             name = Console.ReadLine();
                             break;
                         }
 
-                    case -1:
+                    case 1:
                         {
 
                             Console.WriteLine("enter name");
@@ -42,28 +43,31 @@ namespace Test
                             break;
                         }
 
-                    case -2:
+                    case 2:
                         {
 
                             Console.WriteLine("enter surname");
                             age = int.Parse(Console.ReadLine());
                             break;
                         }
-                    case -3:
+                    case 3:
                         {
 
                             Console.WriteLine("enter age");
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                            Console.WriteLine("");
                             age = int.Parse(Console.ReadLine());
                             break;
                         }
-                    case -4:
+                    case -1:
                         {
 
-                            Console.WriteLine("enter");
+                            Console.WriteLine("enter haight");
                             age = int.Parse(Console.ReadLine());
                             break;
                         }
-                    case -5:
+                    case -2:
                         {
 
                             Console.WriteLine("print exit");

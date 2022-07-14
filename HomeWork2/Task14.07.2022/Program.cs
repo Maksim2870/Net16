962ba14df9b99d9eb2ae158ac25007d227bc921b
Maxim Babich <babich.maxim@bk.ru>
2022-07-14 19:45:16 +0300

    Change Task project location

---
+++ HomeWork2/Task14.07.2022/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            string number = Console.ReadLine();
+            int end = int.Parse(number);
+            int start = 1;
+            int result = 1;
+            int curr = start;
+
+            while (curr <= end)
+
+            {
+                
+                if (curr == end)
+                {
+                    Console.Write(curr + " = ");
+                }
+                else
+                {
+                    Console.Write(curr + " * ");
+                }
+                result = curr * result;
+                curr = (curr + 1);
+            }
+            Console.WriteLine(result);
+        }
+    }
+ }
+
